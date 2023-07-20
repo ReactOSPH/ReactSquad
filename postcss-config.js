74daea0ee65174972ff85b301bd99fd76f12be83
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2023-07-20 22:07:57 +0800

    ugh.

---
+++ postcss-config.js
@@ -0,0 +1,15 @@
+// code from ecomnes/elenacomnes.com
+module.exports = (ctx) => ({
+  map: { inline: false },
+  plugins: {
+    autoprefixer: {},
+    'postcss-import': { root: ctx.file.dirname },
+    'postcss-url': {
+      url: 'copy',
+      useHash: true,
+      assetsPath: 'assets'
+    },
+    'postcss-browser-reporter': {},
+    'postcss-reporter': {}
+  }
+})
